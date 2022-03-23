a58c734f15a756bf1641af03d9b5edea5f2552e6
Ryan Haas <haasrr@etsu.edu>
2022-03-23 00:15:20 -0400

    Added CSS classes demo

---
+++ css_demo/README.rst
@@ -0,0 +1,5 @@
+********************************
+deliverable3 -- CSS Classes Demo
+********************************
+
+`Launch Demo <https://htmlpreview.github.io/?https://github.com/HCI-Groovy-Goobers-II/deliverable3/blob/main/css_demo/src/demo.html>`_
---
+++ css_demo/src/js/student-search.js
@@ -0,0 +1,11 @@
+let studentSearchValues = {
+    valueNames: [
+        "student-name",
+        "student-gradelevel",
+        "student-rating",
+        "student-major",
+        "student-minor"
+    ]
+};
+
+const studentSearchList = new List('student-search-list', studentSearchValues);
