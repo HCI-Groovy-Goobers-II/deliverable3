0f667a3017ffc5fba165b4575824c5d3093820f1
Ryan Haas <haasrr@etsu.edu>
2022-03-26 16:15:19 -0400

    Kill me please

--- django/professors/views.py
+++ django/professors/views.py
@@ -4,13 +4,18 @@ from django.urls import reverse
 from django.shortcuts import render
 
 from users.decorators import allowed_users
+from students.models import Student
+from professors.models import Professor
 
 from datetime import datetime as dt, timedelta
 
 @login_required
 @allowed_users(allowed_groups=['professors'])
 def index(request):
+    prof = Professor.objects.get(user=request.user)
+    students = Student.objects.filter(institution=prof.institution)
     context = {
-
+        'professor': prof,
+        'students': students
     }
     return render(request, 'professors/index.html', context)
